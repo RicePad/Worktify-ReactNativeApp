ca755322579d6bdce71f82dca90871ab1e77efc4
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-25 18:00:49 +0000

    initial react and redux set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
-import UserList from '../containers/UserList';
-import UserDetail from '../containers/UserDetail';
-
 
 class App extends Component {
     
     render(){
-        return(
-            <section>
-                <h1>User List</h1>
-                <UserList />
-                <hr/>
-                <h1>User Detail</h1>
-                <UserDetail />
-            </section>
-            );
-        
-    }   
+        return (
+            <h1>App component</h1>
+            )
+    }
     
 }
-
-
-
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-// For Redux integration
-import { Provider } from 'react-redux';
+
+//Redux Store setup
+import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+
 const store = createStore(reducers);
-const userList = document.getElementById('user-list');
 
 ReactDOM.render(
- <Provider store={store}>       
-    <App />
- </Provider>, userList
-  );
-
-
-
+ <h1>Hello World</h1>, document.getElementById('root')
+ );
